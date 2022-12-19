993fe97cdcce02ded6a0d31050c2e1d7927c0dad
Rahul shah <rahulshah838304@gmail.com>
2022-12-19 23:19:54 +0530

    Lecture 2 Getting Started is added

---
+++ Introduction to Cpp/Lecture 2 Getting Started/Celsius to Fahrenheit.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int cel;
+    float farh;
+    cin>>cel;
+    farh = (5.0 / 9) * (cel - 32);
+    cout<<farh<<endl;
+    return 0;
+}
