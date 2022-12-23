5e93eddfe9a839163f68c37ed38844a298329de5
Rahul shah <rahulshah838304@gmail.com>
2022-12-24 01:02:00 +0530

    Lecture 6 Operator and for loop is added

---
+++ Introduction to Cpp/Lecture 6 Operator and for loop/Increment Decrement Operators.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 2*3+4;
+    cout<<a<<endl;
+
+    int b = 10*5 /9;
+    cout<<a<<endl;
+
+
+/*
+    int a = 10;
+
+    a += 3;
+    cout<< a <<endl;
+    a *= 2;
+    cout<< a <<endl;
+
+    // cout<< ++a <<endl;
+    // cout<< a++ <<endl;
+
+    // cout<< a-- <<endl;
+    // cout<< --a <<endl;
+*/
+}
